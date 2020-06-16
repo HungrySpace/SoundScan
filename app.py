616fb4b6f69a04f1c14e7031a7234157c2b87871
adminmaster <mikirus46@gmail.com>
2020-06-15 21:11:42 -0700

    add db

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///history.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-@app.route('/')
+class OldRec(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # nullable=False - нельзя создавать пустое значение
+    result = db.Column(db.String(30))
+    comment = db.Column(db.String(300))
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<OldRec %r>' % self.id
+
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        comment = request.form['comment']
+        result = 'Null'
+
+        # записываем в бд значения
+        article = OldRec(comment=comment, result=result)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Fail"
+    else:
+        return render_template("index.html")
 
 
 @app.route('/project')
@@ -13,9 +43,14 @@ def project():
     return render_template("project.html")
 
 
+# @app.route('/history')
+# def history():
+#     return render_template("history.html")
+
 @app.route('/history')
 def history():
-    return render_template("history.html")
+    recdb = HistoryComment.query.order_by(HistoryComment.date).all()
+    return render_template("history.html", recdb=recdb)
 
 
 if __name__ == "__main__":
